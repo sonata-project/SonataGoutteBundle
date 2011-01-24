0a23512482c854f9f877174bdb980cf14b69a2f4
Thomas <thomas.rabaix@soleoweb.com>
2011-01-24 11:59:28 +0100

    update to last symfony changes

--- DependencyInjection/GoutteExtension.php
+++ DependencyInjection/GoutteExtension.php
@@ -24,7 +24,8 @@ use Symfony\Component\DependencyInjection\Extension\Extension;
  *
  * @author     Thomas Rabaix <thomas.rabaix@sonata-project.org>
  */
-class GoutteExtension extends Extension {
+class GoutteExtension extends Extension
+{
 
     /**
      * Loads the url shortener configuration.
@@ -32,19 +33,22 @@ class GoutteExtension extends Extension {
      * @param array            $config    An array of configuration settings
      * @param ContainerBuilder $container A ContainerBuilder instance
      */
-    public function configLoad($config, ContainerBuilder $container) {
+    public function configLoad($configs, ContainerBuilder $container)
+    {
 
         // define the page manager
-        $definition = new Definition($config['class']);
-        foreach($config['clients'] as $name => $configuration) {
+        foreach ($configs as $config) {
+            $definition = new Definition($config['class']);
+            foreach ($config['clients'] as $name => $configuration) {
 
-            $configuration['config'] = isset($configuration['config']) ? $configuration['config'] : array();
-            $configuration['server'] = isset($configuration['server']) ? $configuration['server'] : array();
+                $configuration['config'] = isset($configuration['config']) ? $configuration['config'] : array();
+                $configuration['server'] = isset($configuration['server']) ? $configuration['server'] : array();
 
-            $definition->addMethodCall('addClientConfiguration', array($name, $configuration));
+                $definition->addMethodCall('addClientConfiguration', array($name, $configuration));
+            }
+
+            $container->setDefinition('goutte', $definition);
         }
-
-        $container->setDefinition('goutte', $definition);
     }
 
     /**
@@ -52,17 +56,20 @@ class GoutteExtension extends Extension {
      *
      * @return string The XSD base path
      */
-    public function getXsdValidationBasePath() {
+    public function getXsdValidationBasePath()
+    {
 
         return __DIR__.'/../Resources/config/schema';
     }
 
-    public function getNamespace() {
+    public function getNamespace()
+    {
 
         return 'http://www.sonata-project.org/schema/dic/goutte';
     }
 
-    public function getAlias() {
+    public function getAlias()
+    {
 
         return "goutte";
     }
